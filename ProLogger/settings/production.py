17406053853efe449ab2554be59df459b98e30da
nimish <nimish4july1998@gmail.com>
2019-11-11 16:29:05 +0530

    changed production settings

--- ProLogger/settings/production.py
+++ ProLogger/settings/production.py
@@ -1,15 +1,22 @@
 from .base import *
+print("in production")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'prologger',
         'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '0.0.0.0',  # Or an IP Address that your DB is hosted on
-        'PORT': '',
+        'PASSWORD': 'test',
+        'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
         'OPTIONS': {
             # Tell MySQLdb to connect with 'utf8mb4' character set
             'charset': 'utf8mb4',
         },
     }
 }
+
+
+
+
+
+
